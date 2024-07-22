6bb96e987f489ef65326689921e28436540f5b22
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-07-22 23:54:51 +0500

    4- oy 4--dars

---
+++ index.js
@@ -0,0 +1,35 @@
+
+//let sum=0
+//for(let i=10;10<=100;i++){
+//    if(i%2==1){
+//
+//    }
+//    sum=sum+i
+//}
+//console.log(sum);
+//2-misol
+//let sum=0
+//for(let i=1;1<=20;i++){
+//    if(i**2){}
+//    sum=sum+i
+//}
+//ltu= 10
+//for(0+1+1+2+3+5+8+13+21+34){
+ // if(i++,y++,g++,r++)  
+//}
+// let num=10;
+// let sum=0;
+// for (let i=-1;i<=200;i++){
+//    if(i % 2==0){
+//
+//    }
+//    sum=sum+i
+// } 
+// console.log(sum);
+// break//continue
+//let num= 10
+//for(let i=1;1<=10;i++){
+//    if(i==5){
+//        break;
+//    }
+//}
